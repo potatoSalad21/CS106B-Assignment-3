2582f4708b561c776fca88b0cae88110d28acdf2
potatoSalad21 <eeperpeepo@gmail.com>
2025-04-19 22:38:07 +0400

    [4] initial state

--- UniversalHealthcare/UniversalHealthcare.cpp
+++ UniversalHealthcare/UniversalHealthcare.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <vector>
 #include "StanfordCPPLib/console.h"
+#include "StanfordCPPLib/set.h"
 using namespace std;
+
+bool canOfferUniversalCoverage(Set<string>& cities,
+                               vector< Set<string> >& locations,
+                               int numHospitals,
+                               vector< Set<string> >& result);
 
 int main() {
 
     return 0;
 }
+
+/* Function: canOfferUniversalCoverage(Set<string>& cities,
+ *                                     Vector< Set<string> >& locations,
+ *                                     int numHospitals,
+ *                                     Vector< Set<string> >& result);
+ * Usage: if (canOfferUniversalCoverage(cities, locations, 4, result)
+ * ==================================================================
+ * Given a set of cities, a list of what cities various hospitals can
+ * cover, and a number of hospitals, returns whether or not it's
+ * possible to provide coverage to all cities with the given number of
+ * hospitals.  If so, one specific way to do this is handed back in the
+ * result parameter.
+ */
+bool canOfferUniversalCoverage(Set<string>& cities,
+                               vector< Set<string> >& locations,
+                               int numHospitals,
+                               vector< Set<string> >& result) {
+
+    //
+    return false;
+}
+
 
